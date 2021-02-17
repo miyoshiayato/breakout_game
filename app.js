b86d48f45143425ca2654b6b9b9279b03b7e571b
miyoshiayato <miyoshiayato@yahoo.co.jp>
2021-02-17 22:26:42 +0900

    円の色を変更

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var ctx = canvas.getContext("2d");
 
 ctx.beginPath();
 ctx.arc(50, 50, 10, 0, Math.PI * 2, false);
-ctx.fillStyle = "green";
+ctx.fillStyle = "#0095DD";
 ctx.fill();
 ctx.closePath();
 
