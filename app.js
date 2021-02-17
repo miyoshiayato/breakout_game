7b9abc7e44601756441a79011e0e618c69325857
miyoshiayato <miyoshiayato@yahoo.co.jp>
2021-02-17 22:26:09 +0900

    円のサイズ変更

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 
 ctx.beginPath();
-ctx.arc(240, 160, 20, 0, Math.PI * 2, false);
+ctx.arc(50, 50, 10, 0, Math.PI * 2, false);
 ctx.fillStyle = "green";
 ctx.fill();
 ctx.closePath();
