2d29bf1cfed9e1c7f88c22370c7c7524c150feb6
miyoshiayato <miyoshiayato@yahoo.co.jp>
2021-02-17 22:29:09 +0900

    円が中央になるよう配置

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 
+var x = canvas.width / 2;
+var y = canvas.height - 30;
+function draw() {
 ctx.beginPath();
-ctx.arc(50, 50, 10, 0, Math.PI * 2, false);
+ctx.arc(x, y, 10, 0, Math.PI * 2);
 ctx.fillStyle = "#0095DD";
 ctx.fill();
 ctx.closePath();
+}
 
 setInterval(draw, 10);
