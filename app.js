f6c409c8396c42bbd7e2227c5f29a0f97c1f71d7
miyoshiayato <miyoshiayato@yahoo.co.jp>
2021-02-26 21:17:05 +0900

    bricksのxとyの値を反映

--- app.js
+++ app.js
@@ -45,10 +45,13 @@ function drawPaddle() {
 function drawBricks() {
   for(var c = 0; c < brickColumnCount; c++) {
     for(var r = 0; r < brickRowCount; r++) {
+      var brickX = (c * (brickWidth + brickPadding)) + brickOffsetLeft;
+      var brickY = (r * (brickHeight + brickPadding)) + brickOffsetTop;
       bricks[c][r].x = 0;
       bricks[c][r].y = 0;
 
       ctx.beginPath();
+      ctx.rect(brickX, brickY, brickWidth, brickHeight);
       ctx.fillStyle = "#0095DD";
       ctx.fill();
       ctx.closePath();
