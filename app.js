ebb9f2dfc0e5279706753de4efa6692ee47810d5
miyoshiayato <miyoshiayato@yahoo.co.jp>
2021-02-28 16:49:57 +0900

    ブロックにぶつかったらスコアを1加算

--- app.js
+++ app.js
@@ -44,6 +44,7 @@ function collisionDetection() {
         if(x > b.x && x < b.x + brickWidth && y > b.y && y < b.y + brickHeight) {
           dy = -dy;
           b.status = 0;
+          score++;
         }
       }
     }
